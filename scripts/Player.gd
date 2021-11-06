bc8d3103aee337cf84caec669da444ab3b7f5632
AMIOT Yoann <yoann.amiot.dev@gmail.com>
2021-11-06 23:26:24 +0100

    Up player script, bike rotation OK

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,20 +1,25 @@
 extends KinematicBody2D
 
-###Variables###
-var walkSpeed : int = 50;
-var runSpeed : int = walkSpeed * 2;
-var velocity : Vector2 = Vector2.ZERO;
-var isWalking : bool = false;
-var lastDir : String = "down";
-var actualDir : String = "";
-var isRunning : bool = false;
+#General Variables#
+var velocity : Vector2 = Vector2.ZERO
 var transportation : String = "walk";
 
+#Walk Variables#
+var walkSpeed : int = 50;
+var runSpeed : int = walkSpeed * 2;
+var isRunning : bool = false;
+var actualDir : String = "down";
+
+#Bike Variables#
+var bikeSpeed : int = 10;
+var virtualRotation : int = 180;
+var bikeActualDir : String = "down"
 
 
-###Process Function###
+
+#Process Function#
 func _physics_process(delta):
-	if Input.is_action_just_pressed("select_transportation"):
+	if Input.is_action_just_pressed("select_transportation") and velocity == Vector2(0, 0):
 		if transportation == "walk":
 			transportation = "bike";
 		elif transportation == "bike":
@@ -31,127 +36,172 @@ func _physics_process(delta):
 
 
 
-###Function to get the input of the player on foot###
+#Function to get the input of the player on foot#
 func getInputOnFoot():
 	velocity = Vector2.ZERO;
 
 	if Input.is_action_pressed("down"):
 		velocity.y += 1;
 		actualDir = "down";
-		isWalking = true;
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
 		actualDir = "up";
-		isWalking = true;
 	if Input.is_action_pressed("right"):
 		velocity.x += 1;
 		actualDir = "right";
-		isWalking = true;
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1;
 		actualDir = "left";
-		isWalking = true;
-
 	if Input.is_action_pressed("run"):
 		isRunning = true;
+		velocity = velocity.normalized() * runSpeed;
+	else:
+		velocity = velocity.normalized() * walkSpeed;
 
-	if Input.is_action_just_released("down"):
-		isWalking = false;
-		lastDir = "down";
-	if Input.is_action_just_released("up"):
-		isWalking = false;
-		lastDir = "up";
-	if Input.is_action_just_released("right"):
-		isWalking = false;
-		lastDir = "right";
-	if Input.is_action_just_released("left"):
-		isWalking = false;
-		lastDir = "left";
 	if Input.is_action_just_released("run"):
 		isRunning = false;
 
-	if isWalking == true and isRunning == false:
-		velocity = velocity.normalized() * walkSpeed;
-	elif isWalking == true and isRunning == true:
-		velocity = velocity.normalized() * runSpeed;
 
 
 
-###Function to animate the Animated Sprite by testing what the player is doing on foot###
+#Function to animate the Animated Sprite by testing what the player is doing on foot#
 func animateOnFoot():
-	if isWalking == false:
-		if isRunning:
+	if velocity == Vector2(0, 0):
+		if isRunning == true:
 			$AnimatedSprite.animation = "idle_impatient";
-		elif lastDir == "down":
+		elif actualDir == "down":
 			$AnimatedSprite.animation = "idle_downward";
-		elif lastDir == "up":
+		elif actualDir == "up":
 			$AnimatedSprite.animation = "idle_upward";
-		elif lastDir == "right":
+		elif actualDir == "right":
 			$AnimatedSprite.animation = "idle_right";
-		elif lastDir == "left":
+		elif actualDir == "left":
 			$AnimatedSprite.animation = "idle_left";
 
-	elif isWalking == true:
-		if velocity != Vector2(0, 0):
-			if isRunning == false:
-				if actualDir == "down":
-					$AnimatedSprite.animation = "downward_walk";
-				elif actualDir == "up":
-					$AnimatedSprite.animation = "upward_walk";
-				elif actualDir == "right":
-					$AnimatedSprite.animation = "right_walk";
-				elif actualDir == "left":
-					$AnimatedSprite.animation = "left_walk";
-			elif isRunning == true:
-				if actualDir == "down":
-					$AnimatedSprite.animation = "downward_run";
-				elif actualDir == "up":
-					$AnimatedSprite.animation = "upward_run";
-				elif actualDir == "right":
-					$AnimatedSprite.animation = "right_run";
-				elif actualDir == "left":
-					$AnimatedSprite.animation = "left_run";
-
-		elif velocity == Vector2(0, 0):
-			if isRunning == false:
-				$AnimatedSprite.animation = "idle_downward";
-			if isRunning == true:
-				$AnimatedSprite.animation = "idle_impatient";
+	if velocity != Vector2(0, 0):
+		if isRunning == false:
+			if actualDir == "down":
+				$AnimatedSprite.animation = "downward_walk";
+			elif actualDir == "up":
+				$AnimatedSprite.animation = "upward_walk";
+			elif actualDir == "right":
+				$AnimatedSprite.animation = "right_walk";
+			elif actualDir == "left":
+				$AnimatedSprite.animation = "left_walk";
+		elif isRunning == true:
+			if actualDir == "down":
+				$AnimatedSprite.animation = "downward_run";
+			elif actualDir == "up":
+				$AnimatedSprite.animation = "upward_run";
+			elif actualDir == "right":
+				$AnimatedSprite.animation = "right_run";
+			elif actualDir == "left":
+				$AnimatedSprite.animation = "left_run";
 
 
 
-###Function to get the input of the player on bike###
+#Function to get the input of the player on bike#
 func getInputOnBike():
 	velocity = Vector2.ZERO;
 
-	if Input.is_action_just_released("down"):
-		lastDir = "down";
-	if Input.is_action_just_released("up"):
-		lastDir = "up";
-	if Input.is_action_just_released("right"):
-		lastDir = "right";
-	if Input.is_action_just_released("left"):
-		lastDir = "left";
+	if Input.is_action_pressed("down"):
+		pass;
+	if Input.is_action_pressed("up"):
+		pass;
+	if Input.is_action_just_pressed("right"):
+		virtualRotation += 30;
+	if Input.is_action_just_pressed("left"):
+		virtualRotation -= 30;
+
+	if virtualRotation >= 360:
+		virtualRotation = 0;
+
+	if virtualRotation == 0 or virtualRotation == 360:
+		bikeActualDir = "up";
+	elif virtualRotation == 30:
+		bikeActualDir = "up_right";
+	elif virtualRotation == 60:
+		bikeActualDir = "right_up";
+	elif virtualRotation == 90:
+		bikeActualDir = "right";
+	elif virtualRotation == 120:
+		bikeActualDir = "right_down";
+	elif virtualRotation == 150:
+		bikeActualDir = "down_right";
+	elif virtualRotation == 180:
+		bikeActualDir = "down";
+	elif virtualRotation == 210:
+		bikeActualDir = "down_left";
+	elif virtualRotation == 240:
+		bikeActualDir = "left_down";
+	elif virtualRotation == 270:
+		bikeActualDir = "left";
+	elif virtualRotation == 300:
+		bikeActualDir = "left_up";
+	elif virtualRotation == 330:
+		bikeActualDir = "up_left";
+
+	velocity = velocity.normalized() * bikeSpeed;
 
 
 
-###Function to animate the Animated Sprite by testing what the player is doing on bike###
+#Function to animate the Animated Sprite by testing what the player is doing on bike#
 func animateOnBike():
 	if velocity == Vector2(0, 0):
-		if lastDir == "down":
+		#Down#
+		if bikeActualDir == "down":
 			$AnimatedSprite.animation = "bike_idle_downward";
-		elif lastDir == "up":
+		elif bikeActualDir == "down_right":
+			$AnimatedSprite.animation = "bike_idle_downward";
+		elif bikeActualDir == "down_left":
+			$AnimatedSprite.animation = "bike_idle_downward";
+		#Up#
+		elif bikeActualDir == "up":
 			$AnimatedSprite.animation = "bike_idle_upward";
-		elif lastDir == "right":
+		elif bikeActualDir == "up_right":
+			$AnimatedSprite.animation = "bike_idle_upward";
+		elif bikeActualDir == "up_left":
+			$AnimatedSprite.animation = "bike_idle_upward";
+		#Right#
+		elif bikeActualDir == "right":
 			$AnimatedSprite.animation = "bike_idle_right";
-		elif lastDir == "left":
+		elif bikeActualDir == "right_up":
+			$AnimatedSprite.animation = "bike_idle_right";
+		elif bikeActualDir == "right_down":
+			$AnimatedSprite.animation = "bike_idle_right";
+		#Left#
+		elif bikeActualDir == "left":
 			$AnimatedSprite.animation = "bike_idle_left";
-	elif velocity != Vector2(0, 0):
-		if actualDir == "down":
+		elif bikeActualDir == "left_up":
+			$AnimatedSprite.animation = "bike_idle_left";
+		elif bikeActualDir == "left_down":
+			$AnimatedSprite.animation = "bike_idle_left";
+
+	elif velocity != Vector2(0, 0):		#Down#
+		if bikeActualDir == "down":
 			$AnimatedSprite.animation = "bike_slow_pace_downward";
-		elif actualDir == "up":
+		elif bikeActualDir == "down_right":
+			$AnimatedSprite.animation = "bike_slow_pace_downward";
+		elif bikeActualDir == "down_left":
+			$AnimatedSprite.animation = "bike_slow_pace_downward";
+		#Up#
+		elif bikeActualDir == "up":
 			$AnimatedSprite.animation = "bike_slow_pace_upward";
-		elif actualDir == "right":
+		elif bikeActualDir == "up_right":
+			$AnimatedSprite.animation = "bike_slow_pace_upward";
+		elif bikeActualDir == "up_left":
+			$AnimatedSprite.animation = "bike_slow_pace_upward";
+		#Right#
+		elif bikeActualDir == "right":
 			$AnimatedSprite.animation = "bike_slow_pace_right";
-		elif actualDir == "left":
+		elif bikeActualDir == "right_up":
+			$AnimatedSprite.animation = "bike_slow_pace_right";
+		elif bikeActualDir == "right_down":
+			$AnimatedSprite.animation = "bike_slow_pace_right";
+		#Left#
+		elif bikeActualDir == "left":
+			$AnimatedSprite.animation = "bike_slow_pace_left";
+		elif bikeActualDir == "left_up":
+			$AnimatedSprite.animation = "bike_slow_pace_left";
+		elif bikeActualDir == "left_down":
 			$AnimatedSprite.animation = "bike_slow_pace_left";
